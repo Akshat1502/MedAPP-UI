cea419db8fced0d554627e252efc4afb2072b15e
Akshat <Khannaply161@gmail.com>
2023-06-15 14:48:34 +0530

    Dissmissible widget added to make sliding effects to the doctor profiles

--- lib/widget/doctor_profile.dart
+++ lib/widget/doctor_profile.dart
@@ -8,49 +8,79 @@ class DoctorProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-           width: double.infinity,
-      height: 100,
-      decoration: BoxDecoration(
-        color: Colors.black12,
-        borderRadius: BorderRadius.circular(20),
+        
+        Dismissible(
+          onDismissed: (direction) {
+            if(direction==DismissDirection.startToEnd){
+              final lalert=SnackBar(content: Text('Deleted!'),);
+              ScaffoldMessenger.of(context).showSnackBar(lalert);
+              
+            }
+            else{
+              final ralert = SnackBar(content: Text('Message him!'));
+              ScaffoldMessenger.of(context).showSnackBar(ralert);
 
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(borderRadius: BorderRadius.all(Radius.circular(10)),child: Image.asset('assets/images/doctor1.jpg',height: 80,width: 120,)),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 8,left: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Dr. Roshan Lal Chaddha',style:TextStyle(fontSize: 16,fontWeight: FontWeight.bold,)),
-                  const SizedBox(height: 3,),
-                  Text('Tooth Specialist',style:TextStyle(fontSize: 13,fontWeight: FontWeight.normal,) ,),
-                  const SizedBox(height: 3,),
-                  Row(
-                    children: [
-                      Icon(Ionicons.star,color: Colors.amber,size: 20,),
-                      const SizedBox(width: 5,),
-                      Text('4.3',style:TextStyle(fontSize: 13,fontWeight: FontWeight.normal,) ,),
-                      const SizedBox(width: 30,),
-                      Icon(Ionicons.time,color: Colors.blue,size: 20,),
-                      const SizedBox(width: 5,),
-                      Text('11 Am - 3 Pm',style:TextStyle(fontSize: 13,fontWeight: FontWeight.normal,) ,),
-                    ],
-                  ),
-                  
-                ],
-              ),
+            }
+            
+          },
+          key: Key('Text'),
+          background:Container(color: Colors.red,),
+          
+          secondaryBackground: Container(color: Colors.blue,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Ionicons.chatbubble_ellipses,size: 30,),
+              ],
             ),
-          ],
-        ),
-      ),
-       
+          ),),   
+                 child: Container(
+             width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.all(Radius.circular(20))
+        
+              ),
+              child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(borderRadius: BorderRadius.all(Radius.circular(10)),child: Image.asset('assets/images/doctor1.jpg',height: 80,width: 120,)),
+        
+              Padding(
+                padding: const EdgeInsets.only(top: 8,left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Dr. Roshan Lal Chaddha',style:TextStyle(fontSize: 16,fontWeight: FontWeight.bold,)),
+                    const SizedBox(height: 3,),
+                    Text('Tooth Specialist',style:TextStyle(fontSize: 13,fontWeight: FontWeight.normal,) ,),
+                    const SizedBox(height: 3,),
+                    Row(
+                      children: [
+                        Icon(Ionicons.star,color: Colors.amber,size: 20,),
+                        const SizedBox(width: 5,),
+                        Text('4.3',style:TextStyle(fontSize: 13,fontWeight: FontWeight.normal,) ,),
+                        const SizedBox(width: 30,),
+                        Icon(Ionicons.time,color: Colors.blue,size: 20,),
+                        const SizedBox(width: 5,),
+                        Text('11 Am - 3 Pm',style:TextStyle(fontSize: 13,fontWeight: FontWeight.normal,) ,),
+                      ],
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
+              ),
+               
+          ),
         ),
         const SizedBox(height: 20,),
 
